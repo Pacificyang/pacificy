67be270d66f8ef37e19460a94dc9e1fa9af21a66
Pacificyang <yangtaiping@gmail.com>
2020-11-26 16:25:41 +0800

    ready

--- src/com/pacificy/hi.java
+++ src/com/pacificy/hi.java
@@ -1,7 +1,12 @@
 package com.pacificy;
+import java.util.Scanner;
 
 public class hi {
     public static void main(String[] args) {
         System.out.println("hihi");
+        Scanner sc = new Scanner(System.in);
+        String next = sc.next();
+        System.out.println(next);
+
     }
 }
